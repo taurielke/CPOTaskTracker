3770eb517d3e2c2bd55dc2035979b7f8dc8dc2de
Kate <taurielkee@yandex.ru>
2023-12-13 08:18:55 +0400

    завершение тестов

--- CourseWorkTaskTracker/app/src/test/java/com/example/courseworktasktracker/ExampleUnitTest.java
+++ CourseWorkTaskTracker/app/src/test/java/com/example/courseworktasktracker/ExampleUnitTest.java
@@ -1,7 +1,9 @@
 package com.example.courseworktasktracker;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -18,6 +20,8 @@ import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.example.courseworktasktracker.data.Category;
 import com.example.courseworktasktracker.data.CategoryData;
+import com.example.courseworktasktracker.data.Task;
+import com.example.courseworktasktracker.data.TaskData;
 
 import java.util.List;
 
@@ -29,15 +33,19 @@ import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = Build.VERSION_CODES.P)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleUnitTest {
 
     private CategoryData categoryData;
+    private TaskData taskData;
     private Context context;
 
     @Before
     public void setUp() {
         context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         categoryData = new CategoryData(context);
+        taskData = new TaskData(context);
+        taskData.deleteAll();
         categoryData.deleteAll();
     }
 
@@ -97,6 +105,59 @@ public class ExampleUnitTest {
         categoryData.addCategory("TestCategory");
         categoryData.deleteCategory(1);
         List<Category> result = categoryData.findAllCategory();
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetTask() {
+        Task task = new Task();
+        task.setId(1);
+        taskData.addTask("test", 3);
+        Task result = taskData.getTask(1);
+        assertNotNull(result);
+        assertEquals(1, result.getId());
+        assertEquals("test", result.getName());
+        assertEquals(3, result.getCategoryId());
+    }
+
+    @Test
+    public void testFindAllTasks() {
+        taskData.addTask("TestTask", 4);
+        taskData.addTask("TestTask1", 4);
+        List<Task> result = taskData.findAllTask();
+        assertNotNull(result);
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testAddTask() {
+        taskData.addTask("Task", 1);
+        List<Task> result = taskData.findAllTask();
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("Task", result.get(0).getName());
+    }
+
+    @Test
+    public void testUpdateTask() {
+        Task task = new Task();
+        task.setId(1);
+        taskData.addTask("testTask", 5);
+        taskData.updateTask(1, "updatedTask",5) ;
+        Task updatedTask = taskData.getTask(1);
+        assertNotNull(updatedTask);
+        assertEquals(1, updatedTask.getId());
+        assertEquals("updatedTask", updatedTask.getName());
+        assertEquals(5, updatedTask.getCategoryId());
+    }
+
+    @Test
+    public void testDeleteTask() {
+        Task task = new Task();
+        task.setId(1);
+        taskData.addTask("testTask", 5);
+        taskData.deleteTask(1);
+        List<Task> result = taskData.findAllTask();
         assertTrue(result.isEmpty());
     }
 }
